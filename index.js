092fc6c5dcccd0323d519001876722780024f403
Cameron C <ciempolacameron@ggmail.com>
2026-03-05 19:44:56 -0500

    Changed Description

--- index.js
+++ index.js
@@ -174,28 +174,36 @@ client.once("ready", async () => {
   }
 });
 
-// ---------- WELCOME SYSTEM ----------
+// ---------- WELCOME ----------
+
 client.on("guildMemberAdd", async member => {
   try {
     const channel = await client.channels.fetch(WELCOME_CHANNEL_ID);
     if (!channel) return;
 
     const embed = new EmbedBuilder()
-      .setColor(0x4587ff)
-      .setTitle("Welcome to the Server!")
-      .setDescription(`Welcome ${member} to **${member.guild.name}**!`)
-      .setThumbnail(member.user.displayAvatarURL({ size: 512 }))
-      .addFields({
-        name: "Member Count",
-        value: `${member.guild.memberCount}`,
-        inline: true
-      })
-      .setTimestamp();
+     .setColor(0x4587ff)
+.setTitle(`Welcome to **${member.guild.name}** ${member}`)
+.setDescription(`**Welcome To Sylix!**
+<:discordemoji:1479274884809883762> Check out our [website](https://sylix.cc/) if you are interested in purchasing something
+<:discordemoji:1479274884809883762> If you need support please make a [ticket](https://discord.com/channels/1463364200540799040/1465800232502825275) after verifying
+<:discorde:1479274851444330570> Make sure to read all of the [rules](https://discord.com/channels/1463364200540799040/1465937574169411686) before chatting`)
+      .setThumbnail("https://i.ibb.co/ymn10dMY/your-image.png")
+      .addFields([
+        {
+          name: "Member Count",
+          value: `${member.guild.memberCount}`,
+          inline: true
+        }
+      ])
+      .setFooter({
+        text: "Sylix • Welcome System",
+        iconURL: member.guild.iconURL()
+      });
 
     await channel.send({ embeds: [embed] });
-
-  } catch (err) {
-    console.error("Welcome error:", err);
+  } catch (error) {
+    console.error("Error sending welcome message:", error);
   }
 });
 
