5a55bda75623fce841e8ca9755368a10374e1e58
Cameron C <ciempolacameron@ggmail.com>
2026-03-11 18:24:59 -0400

    first commit

--- index.js
+++ index.js
@@ -214,6 +214,7 @@ client.on("guildMemberAdd", async member => {
       .setTitle(`**hi**`)
       .setDescription(
 `**<:sylix:1468005258126163990> Welcome To Sylix.cc <@${member.id}>**
+<:discordemoji:1479274884809883762> Please make sure to [verify](https://discord.com/channels/1463364200540799040/1465839281808609381) to gain full access
 <:discordemoji:1479274884809883762> Check out our [website](https://sylix.cc/)
 <:discordemoji:1479274884809883762> If you need support please make a [ticket](https://discord.com/channels/1463364200540799040/1465800232502825275)
 <:discorde:1479274851444330570> Make sure to read all of the [rules](https://discord.com/channels/1463364200540799040/1465937574169411686)`
